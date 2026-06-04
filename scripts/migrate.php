95083e05d1b79777d25749a27b67163dd91ffe50
H. S. L. Silva <sandaralasandi03@gmail.com>
2026-06-04 15:00:54 +0530

    Create Team.php

--- scripts/migrate.php
+++ scripts/migrate.php
@@ -1,12 +1,22 @@
 <?php
 require 'vendor/autoload.php';
 require "src/Models/User.php";
+require "src/Models/Team.php";
 
 use Models\User;
 
 try {
     User::createClass();
     echo "User Table created successfully.";
+} catch (Exception $e) {
+    echo "Error creating table: " . $e->getMessage();
+}
+
+use Models\Team;
+
+try {
+    Team::createClass();
+    echo "Team Table created successfully.";
 } catch (Exception $e) {
     echo "Error creating table: " . $e->getMessage();
 }
---
+++ src/Models/Team.php
@@ -0,0 +1,57 @@
+<?php
+namespace Models;
+
+require 'BaseModel.php';
+
+use Models\BaseModel;
+use database\Column;
+use database\Table;
+use DateTime;
+
+#[Table(name: 'Teams')]
+class Team extends BaseModel
+{
+  #[Column(type: 'INT', nullable: false, primaryKey: true, autoIncrement: true)]
+  protected int $id;
+
+  #[Column(type: 'INT', nullable: false)]
+  protected int $eventId;
+
+  #[Column(type: 'VARCHAR', length: 150, nullable: false)]
+  protected string $teamName;
+
+  #[Column(type: 'INT', nullable: false)]
+  protected int $managedBy;
+
+  #[Column(type: 'DATETIME', default: 'CURRENT_TIMESTAMP')]
+  protected DateTime $createdAt;
+
+  #[Column(type: 'INT', nullable: false)]
+  protected DateTime $createdBy;
+
+  public function __construct($eventId, $teamName, $managedBy, $createdBy) {
+    $this->eventId = $eventId;
+    $this->teamName = $teamName;
+    $this->managedBy = $managedBy;
+    $this->createdBy = $createdBy;
+    $this->createdAt = new DateTime();
+    parent::__construct();
+  } 
+  
+  public static function empty() : self {
+    return new self(0,"",0,0);
+  }
+  
+
+
+
+
+
+
+
+
+
+
+
+}
+?>
