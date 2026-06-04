c29ed069b89b3589a8e205337a2f5fee63119b5d
Irudini-nawoda <irudini14@gmail.com>
2026-06-04 15:10:20 +0530

    created:Tasks model

---
+++ src/Models/Task.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Models;
+
+require 'BaseModel.php';
+
+use Models\BaseModel;
+use database\Column;
+use database\Table;
+use DateTime;
+
+#[Table(name: 'tasks')]
+class Task extends BaseModel
+{
+    #[Column(type: 'INT', nullable: false, primaryKey: true, autoIncrement: true)]
+    protected int $id;
+
+    #[Column(type: 'INT', nullable: false)]
+    protected int $eventId;
+
+    #[Column(type: 'VARCHAR', length: 255, nullable: false)]
+    protected string $title;
+
+    #[Column(type: 'TEXT', nullable: true)]
+    protected ?string $description = null;
+
+    #[Column(type: 'INT', nullable: false)]
+    protected int $assignedTo;
+
+    #[Column(type: 'INT', nullable: false)]
+    protected int $assignedBy;
+
+    #[Column(type: 'DATETIME', nullable: true)]
+    protected ?DateTime $createddAt = null;
+
+    #[Column(type: 'DATETIME', nullable: true)]
+    protected ?DateTime $assignedDate = null;
+
+    #[Column(type: 'DATETIME', nullable: true)]
+    protected ?DateTime $dueDate = null;
+
+    #[Column(type: 'DATETIME', nullable: true)]
+    protected ?DateTime $updatedAt = null;
+
+    #[Column(type: 'VARCHAR', length: 255, nullable: false, default: "TODO")]
+    protected string $status = "TODO";
+
+    public function __construct($eventId, $title, $description, $assignedTo, $assignedBy, $dueDate,$status = 'TODO') {
+        $this->eventId = $eventId;
+        $this->title = $title;
+        $this->description = $description;
+        $this->assignedTo = $assignedTo;
+        $this->assignedBy = $assignedBy;
+        $this->createddAt = new DateTime();
+        $this->assignedDate = new DateTime($assignedDate);
+        $this->dueDate = new DateTime($dueDate);
+        $this->updatedAt = new DateTime();
+
+        parent::__construct();
+    }
+
+    public static function empty() : self {
+        return new self("", "", "", "", "", "");
+    }
+}
