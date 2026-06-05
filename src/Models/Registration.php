a440bc026bdbbf652b294e1930f9be8452545c78
H. S. L. Silva <sandaralasandi03@gmail.com>
2026-06-05 19:32:17 +0530

    create Registration table

--- src/Models/Registration.php
+++ src/Models/Registration.php
@@ -18,12 +18,43 @@ class Registration extends BaseModel
   #[Column(type: 'INT', nullable: false)]
   protected int $userId;
 
+<<<<<<< Updated upstream
   #[Column(type: 'DATETIME', default: 'CURRENT_TIMESTAMP')]
   protected DateTime $registeredAt;
+=======
+  #[Column(type: 'VARCHAR', length: 50, nullable: false)]
+  protected string $status;
+
+  #[Column(type: 'DATETIME', default: 'CURRENT_TIMESTAMP')]
+  protected DateTime $createdAt;
+
+  #[Column(type: 'DATETIME', default: 'CURRENT_TIMESTAMP')]
+  protected DateTime $updatedAt;
+
+  public function __construct(int $eventId, int $userId, string $status = 'pending')
+  {
+    $this->eventId = $eventId;
+    $this->userId = $userId;
+    $this->status = $status;
+    $this->createdAt = new DateTime();
+    $this->updatedAt = new DateTime();
+
+    parent::__construct();
+  }
+
+  public static function empty(): self
+  {
+    return new self(0, 0, "");
+  }
+
+}
+  
+>>>>>>> Stashed changes
 
   #[Column(type: 'VARCHAR', length: 255, nullable: false)]
   protected string $name;
 
+<<<<<<< Updated upstream
   #[Column(type: 'VARCHAR', length: 255, nullable: false)]
   protected string $email;
 
@@ -48,3 +79,9 @@ class Registration extends BaseModel
     return new self(0,0,"","","","");
   }
 }
+=======
+
+  
+
+ 
+>>>>>>> Stashed changes
