a03ce3a4f06def44b66a8f1736b22a8039032121
P. P. C. Fernando <pramodfernando49@gmail.com>
2026-06-30 22:44:30 +0530

    Add update event route and refactor event data handling

--- Routes/events.php
+++ Routes/events.php
@@ -8,3 +8,4 @@ $eventController = new EventController();
 $router->get("/events", [$eventController, "listEvents"]);
 $router->get("/event", [$eventController, "getEventDetails"]);
 $router->post("/event", [$eventController, "createEvent"], [AuthMiddleware::class]);
+$router->put("/event", [$eventController, "updateEvent"], [AuthMiddleware::class]);
