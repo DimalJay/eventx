1523f3f7621d6a104b09f1eb5db0e3bc4b249557
H. S. L. Silva <sandaralasandi03@gmail.com>
2026-06-30 17:58:23 +0530

    update discover events

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ require_once __DIR__ . '/Routes/feedbacks.php';
 require_once __DIR__ . '/Routes/events.php';
 require_once __DIR__ . '/Routes/tasks.php';
 require_once __DIR__ . '/Routes/teamAccess.php';
-require_once __DIR__ . '/Routes/discoverEvents.php';
+// require_once __DIR__ . '/Routes/discoverEvents.php';
 
 $router->dispatch();
 ?>
