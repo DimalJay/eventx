58fc5bce64815d6aa1909d5eacd28564896e47ba
Dimal Kavindu J. <80914328+DimalJay@users.noreply.github.com>
2026-07-09 20:51:51 +0530

    update

--- index.php
+++ index.php
@@ -13,5 +13,14 @@ require_once __DIR__ . '/Routes/events.php';
 require_once __DIR__ . '/Routes/tasks.php';
 require_once __DIR__ . '/Routes/teamAccess.php';
 
+$path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+$file = __DIR__ . $path;
+
+if (file_exists($file) && !is_dir($file)) {
+    return false; 
+}
+
+$_GET['request'] = ltrim($path, '/');
+require 'index.php';
+
 $router->dispatch();
-?>
