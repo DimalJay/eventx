adc1024fbab1f4d3f3292b85cc34320463a7a5ca
pramodchandima <pramodfernando49@gmail.com>
2026-08-15 18:19:29 +0530

    Fix check-in email timezone and redesign reminder email template

--- index.php
+++ index.php
@@ -16,5 +16,6 @@ require_once __DIR__ . '/Routes/tasks.php';
 require_once __DIR__ . '/Routes/teamAccess.php';
 require_once __DIR__ . '/Routes/registration.php';
 require_once __DIR__ . '/Routes/admin.php';
+require_once __DIR__ . '/Routes/cron.php';
 
 $router->dispatch();
