7befdd8493ee248a864f9e8a8d40074c3b5125d4
Dimal Kavindu J. <80914328+DimalJay@users.noreply.github.com>
2026-08-28 00:22:07 +0530

    update

--- Routes/auth.php
+++ Routes/auth.php
@@ -1,6 +1,7 @@
 <?php
 use Controllers\UserController;
 use Controllers\AuthController;
+use Middlewares\AuthMiddleware;
 
 $userController = new UserController();
 $authController = new AuthController();
@@ -10,3 +11,4 @@ $router->post("/auth/login", [$authController, "login"]);
 $router->post("/auth/admin-login", [$authController, "adminLogin"]);
 $router->post("/auth/logout", [$authController, "logout"]);
 $router->post("/auth/google-login", [$authController, "googleLogin"]);
+$router->post("/auth/change-password", [$authController, "changePassword"], [AuthMiddleware::class]);
